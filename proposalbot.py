4d16b87d2c8364458dfcd5091005234e1d72bd5f
chancee12 <128835183+chancee12@users.noreply.github.com>
2023-07-27 14:57:29 -0400

    Update proposalbot.py

--- proposalbot.py
+++ proposalbot.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit_chat import message
 from PIL import Image
 import openai
 from langchain.chains import ConversationChain
@@ -36,19 +35,18 @@ def on_input_change():
     user_input = st.session_state.user_input
     st.session_state.past.append(user_input)
     if user_input:
-        if user_input:
-            prompt_mapping = {
-                "Custom Prompt": user_input,
-                "Create Acronym List": f"Please create a deduplicated list of acronyms from the following text: '{user_input}'",
-                "Identify Key Proposal Requirements": f"Identify key requirements from the following proposal text: '{user_input}'",
-                "Summarize GIS Service Description": f"Provide a concise summary of the following GIS service description: '{user_input}'",
-                "Simplify Complex GIS Language": f"Simplify the following complex GIS-related language for a non-technical audience: '{user_input}'",
-                "Summarize Proposal Section": f"Provide a brief summary of the following proposal section: '{user_input}'",
-                "Revise Proposal Text": f"Revise the following proposal text for improved clarity and effectiveness: '{user_input}'",
-                "Identify Improvement Areas": f"Identify potential areas for improvement in the following proposal text: '{user_input}'",
-                "Respond to RFP Questions": f"Generate a response to the following GIS-related RFP question: '{user_input}'",
-                "Analyze Technical Requirements": f"Analyze the following technical requirements and provide an evaluation: '{user_input}'"
-            }
+        prompt_mapping = {
+            "Custom Prompt": user_input,
+            "Create Acronym List": f"Please create a deduplicated list of acronyms from the following text: '{user_input}'",
+            "Identify Key Proposal Requirements": f"Identify key requirements from the following proposal text: '{user_input}'",
+            "Summarize GIS Service Description": f"Provide a concise summary of the following GIS service description: '{user_input}'",
+            "Simplify Complex GIS Language": f"Simplify the following complex GIS-related language for a non-technical audience: '{user_input}'",
+            "Summarize Proposal Section": f"Provide a brief summary of the following proposal section: '{user_input}'",
+            "Revise Proposal Text": f"Revise the following proposal text for improved clarity and effectiveness: '{user_input}'",
+            "Identify Improvement Areas": f"Identify potential areas for improvement in the following proposal text: '{user_input}'",
+            "Respond to RFP Questions": f"Generate a response to the following GIS-related RFP question: '{user_input}'",
+            "Analyze Technical Requirements": f"Analyze the following technical requirements and provide an evaluation: '{user_input}'"
+        }
 
         Conversation = ConversationChain(
             llm=llm, 
@@ -56,11 +54,9 @@ def on_input_change():
             memory=st.session_state.entity_memory
         )
 
-        output = Conversation(user_input)
-        # Add to the conversation history
+        output = Conversation.complete(prompt_mapping[st.session_state.selected_task])
         st.session_state.conversation_history += f"\nUser: {user_input}\nAI: {output}"
         st.session_state.generated.append(f"AI: {output}")
-    
     st.session_state.user_input = ''
 
 def on_btn_click():
@@ -131,14 +127,14 @@ if check_password():
     ]
     st.session_state.selected_task = st.selectbox("Select a task:", task_options, key='selected_task')
     
+    def display_chat():
+        for i in range(len(st.session_state['past'])):
+            st.markdown(f"**User**: {st.session_state['past'][i]}")
+            st.markdown(f"**AI**: {st.session_state['generated'][i]}")
+
     with st.container():
         st.text_input("User Input:", on_change=on_input_change, key="user_input")
-        
-    chat_placeholder = st.empty()
 
-    with chat_placeholder.container():    
-        for i in range(len(st.session_state['generated'])):                
-            message(st.session_state['past'][i], is_user=True, key=f"{i}_user")
-            message(st.session_state['generated'][i], key=f"{i}")
-    
-        st.button("Clear message", on_click=on_btn_click)
+    display_chat()
+
+    st.button("Clear message", on_click=on_btn_click)

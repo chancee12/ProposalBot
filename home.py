87cca2e776fb18ad7decd3083e4d3b4cd9a9bad9
chancee12 <128835183+chancee12@users.noreply.github.com>
2023-08-01 13:14:47 -0400

    removelinks

--- home.py
+++ home.py
@@ -8,10 +8,6 @@ st.set_page_config(
 
 st.header("Chatbot Implementations with Langchain")
 st.write("""
-[![view source code ](https://img.shields.io/badge/GitHub%20Repository-gray?logo=github)](https://github.com/shashankdeshpande/langchain-chatbot)
-![Visitors](https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Flangchain-chatbot.streamlit.app&label=Visitors&labelColor=%235d5d5d&countColor=%231e7ebf&style=flat)
-""")
-st.write("""
 Langchain is a powerful framework designed to streamline the development of applications using Language Models (LLMs). It provides a comprehensive integration of various components, simplifying the process of assembling them to create robust applications.
 
 Leveraging the power of Langchain, the creation of chatbots becomes effortless. Here are a few examples of chatbot implementations catering to different use cases:

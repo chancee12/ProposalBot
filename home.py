2160e1b9ee353c642747764d7777beec9250f0b9
chancee12 <128835183+chancee12@users.noreply.github.com>
2023-08-01 13:23:16 -0400

    addedgpt4

--- home.py
+++ home.py
@@ -6,16 +6,24 @@ st.set_page_config(
     layout='wide'
 )
 
-st.header("Chatbot Implementations with Langchain")
+st.header("Langchain Chatbot Implementations at Axim")
+st.sidebar.title("Contact")
+st.sidebar.info(
+    """
+    Chancee Vincent:
+    [LinkedIn](www.linkedin.com/in/chancee-vincent-4371651b6)
+    """
+)
+
 st.write("""
-Langchain is a powerful framework designed to streamline the development of applications using Language Models (LLMs). It provides a comprehensive integration of various components, simplifying the process of assembling them to create robust applications.
+Langchain is an effective framework conceived to expedite the development of applications powered by Language Models (LLMs). This open-source model delivers a well-rounded integration of diverse components, thereby streamlining the assembly process for robust application creation.
 
-Leveraging the power of Langchain, the creation of chatbots becomes effortless. Here are a few examples of chatbot implementations catering to different use cases:
+With Langchain, we have significantly simplified chatbot creation. Here are several examples of chatbot implementations serving different needs:
 
-- **Basic Chatbot**: Engage in interactive conversations with the LLM.
-- **Context aware chatbot**: A chatbot that remembers previous conversations and provides responses accordingly.
-- **Chatbot with Internet Access**: An internet-enabled chatbot capable of answering user queries about recent events.
-- **Chat with your documents** Empower the chatbot with the ability to access custom documents, enabling it to provide answers to user queries based on the referenced information.
+- **Basic Chatbot**: Facilitates interactive conversations with the LLM.
+- **Context-aware Chatbot**: A chatbot capable of retaining past conversation context to produce more relevant responses.
+- **Internet-Accessible Chatbot**: This chatbot can access the internet to answer user inquiries about current events.
+- **Document-Integrated Chatbot**: This chatbot has the ability to reference custom documents, enabling it to respond to user queries with information from specific documents.
 
-To explore sample usage of each chatbot, please navigate to the corresponding chatbot section.
+To explore the functionality of each chatbot, please navigate to the respective chatbot section.
 """)
--- pages/01_🤖_basic_chatbot.py
+++ pages/01_🤖_basic_chatbot.py
@@ -13,8 +13,8 @@ class Basic:
 
     def __init__(self):
         utils.configure_openai_api_key()
-        self.openai_model = "gpt-3.5-turbo"
-    
+        self.openai_model = st.selectbox('Select LLM model', ('gpt-3.5-turbo', 'gpt-4'))
+
     def setup_chain(self):
         llm = OpenAI(model_name=self.openai_model, temperature=0, streaming=True)
         chain = ConversationChain(llm=llm, verbose=True)

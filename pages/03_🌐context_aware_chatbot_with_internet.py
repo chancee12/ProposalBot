8dcefd7ff28e515b2f60dfd13a93751dc9278cdd
chancee12 <128835183+chancee12@users.noreply.github.com>
2023-09-18 15:55:38 -0700

    Update 03_🌐context_aware_chatbot_with_internet.py

--- pages/03_🌐context_aware_chatbot_with_internet.py
+++ pages/03_🌐context_aware_chatbot_with_internet.py
@@ -1,12 +1,10 @@
-import utils
 import streamlit as st
-from langchain.llms import OpenAI
-from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferMemory
-from langchain.agents import AgentType, initialize_agent, Tool
-from langchain.chat_models import ChatOpenAI
-from langchain.tools import DuckDuckGoSearchRun
+from langchain.agents import ConversationalChatAgent, AgentExecutor
 from langchain.callbacks import StreamlitCallbackHandler
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
+from langchain.tools import DuckDuckGoSearchRun
 
 def check_password():
     """Returns `True` if the user had the correct password."""
@@ -37,34 +35,52 @@ def check_password():
 
 if check_password():
 
-    st.set_page_config(page_title="ChatWeb", page_icon="🌐")
-    st.header('Chatbot with Internet Access')
-    st.write('Equipped with internet access, enables users to ask questions about recent events')
+    st.set_page_config(page_title="LangChain: Chat with search", page_icon="🦜")
+    st.title("🦜 LangChain: Chat with search")
 
-    class ChatbotTools:
+    openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+    openai_model = st.sidebar.selectbox('Select LLM model', ('gpt-4', 'gpt-3.5-turbo'))
 
-        def __init__(self):
-            utils.configure_openai_api_key()
-            self.openai_model = st.selectbox('Select LLM model', ('gpt-4', 'gpt-3.5-turbo'))
-            self.memory = ConversationBufferMemory(return_messages=True, memory_key="chat_history", output_key="output")  # Updated line
+    msgs = StreamlitChatMessageHistory()
+    memory = ConversationBufferMemory(
+        chat_memory=msgs, return_messages=True, memory_key="chat_history", output_key="output"
+    )
+    if len(msgs.messages) == 0 or st.sidebar.button("Reset chat history"):
+        msgs.clear()
+        msgs.add_ai_message("How can I help you?")
+        st.session_state.steps = {}
 
-        def setup_chain(self):
-            llm = ChatOpenAI(model_name=self.openai_model, streaming=True)
-            chain = ConversationChain(llm=llm, memory=self.memory, verbose=True)  # Include memory object in chain setup
-            return chain
+    avatars = {"human": "user", "ai": "assistant"}
+    for idx, msg in enumerate(msgs.messages):
+        with st.chat_message(avatars[msg.type]):
+            # Render intermediate steps if any were saved
+            for step in st.session_state.steps.get(str(idx), []):
+                if step[0].tool == "_Exception":
+                    continue
+                with st.status(f"**{step[0].tool}**: {step[0].tool_input}", state="complete"):
+                    st.write(step[0].log)
+                    st.write(step[1])
+            st.write(msg.content)
 
-        @utils.enable_chat_history
-        def main(self):
-            chain = self.setup_chain()
-            user_query = st.chat_input(placeholder="Ask me anything!")
-            if user_query:
-                utils.display_msg(user_query, 'user')
-                with st.chat_message("assistant"):
-                    st_cb = StreamlitCallbackHandler(st.container())
-                    response = chain.run(user_query, callbacks=[st_cb])
-                    st.session_state.messages.append({"role": "assistant", "content": response})
-                    st.write(response)
+    if prompt := st.chat_input(placeholder="Who won the Women's U.S. Open in 2018?"):
+        st.chat_message("user").write(prompt)
 
-    if __name__ == "__main__":
-        obj = ChatbotTools()
-        obj.main()
+        if not openai_api_key:
+            st.info("Please add your OpenAI API key to continue.")
+            st.stop()
+
+        llm = ChatOpenAI(model_name=openai_model, openai_api_key=openai_api_key, streaming=True)
+        tools = [DuckDuckGoSearchRun(name="Search")]
+        chat_agent = ConversationalChatAgent.from_llm_and_tools(llm=llm, tools=tools)
+        executor = AgentExecutor.from_agent_and_tools(
+            agent=chat_agent,
+            tools=tools,
+            memory=memory,
+            return_intermediate_steps=True,
+            handle_parsing_errors=True,
+        )
+        with st.chat_message("assistant"):
+            st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+            response = executor(prompt, callbacks=[st_cb])
+            st.write(response["output"])
+            st.session_state.steps[str(len(msgs.messages) - 1)] = response["intermediate_steps"]
